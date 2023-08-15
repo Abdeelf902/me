fdee3332d7f6ac1b4f78de50443fdf33a0d176f1
Abderrahmane El farouah <103990456+Abdeelf902@users.noreply.github.com>
2023-08-15 16:31:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Récupération des éléments du DOM
 const menuIcon = document.querySelector('.menu-icon');
-const navList = document.querySelector('.nav-list ul');
+const navList = document.querySelector('.nav-list');
 
 // Ajout d'un gestionnaire d'événement pour le clic sur l'icône du menu
 menuIcon.addEventListener('click', () => {
