6f2c77630b61484d716edde5bbb5d472996bb2fc
abderrahmane el farouah <abderrahmane.elfarouah@laposte.net>
2023-08-18 16:36:53 +0200

    fix  style+nav

--- script.js
+++
@@ -1,8 +0,0 @@
-// Récupération des éléments du DOM
-const menuIcon = document.querySelector('.menu-icon');
-const subMenu = document.querySelector('.sub-menu');
-
-// Ajout d'un gestionnaire d'événement pour le clic sur l'icône du menu
-menuIcon.addEventListener('click', () => {
-    subMenu.classList.toggle('active');
-});
