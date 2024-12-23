707115c933bba4ab5aaf8294de1efac54756a2c0
Abderrahmane El farouah <103990456+abdeelfarouah@users.noreply.github.com>
2024-12-23 15:13:42 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
-            <span className="text-xl font-bold text-gray-900">Portofolio Abderrahmane EL FAROUAH </span>
+            <span className="text-xl font-bold text-gray-900">My Portfolio </span>
           </div>
           
           <div className="hidden md:block">
