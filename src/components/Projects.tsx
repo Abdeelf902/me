af810edd3158a26794f56b45689cd5ddce50f0fc
abdeelfarouah <abderrahmane.elfarouah@laposte.net>
2025-01-13 15:37:09 +0100

    project add and fix

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,11 +5,25 @@ import ProjectCard from './projects/ProjectCard';
 
 const projects = [
   {
+    title: 'Snake game legacy ',
+    description: 'jeu regressif du serpent.',
+    link: 'https://snakegamelegacy.netlify.app',
+    github: 'https://github.com/abdeelfarouah/retro-snake-game',
+    image: PROJECT_IMAGES.SNAKE,
+  },
+  {
     title: 'Chi Fu Mi',
     description: 'Un jeu classique de pierre-papier-ciseaux développé avec JavaScript.',
     link: 'https://abdeelfarouah.github.io/chifoumi/',
-    github: 'https://github.com/Abdeelf902/chifoumi',
+    github: 'https://github.com/abdeelfarouah/chifoumi',
     image: PROJECT_IMAGES.CHIFOUMI,
+  },
+  {
+    title: 'Neo Puzzle',
+    description: 'Un jeu simple revisité du puzzle .',
+    link: 'https://abdeelfarouah.github.io/puzzle/',
+    github: 'https://github.com/abdeelfarouah/puzzle',
+    image: PROJECT_IMAGES.PUZZLE,
   },
   {
     title: 'Pokédex',
