eb7c1bf498c8f9b173abd994e93191f6360f326f
abdeelfarouah <abderrahmane.elfarouah@laposte.net>
2025-02-12 16:07:16 +0100

    fix: mettre à jour l'URL du CV dans les constantes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,7 @@ export const SOCIAL_LINKS = {
   GITHUB: 'https://github.com/abdeelfarouah/',
   LINKEDIN: 'https://fr.linkedin.com/in/abderrahmaneelfarouah',
   EMAIL: 'A.elfarouahDEV@outlook.fr',
-  CV_URL: 'src/abderrahmane EL FAROUAH CV.pdf',
+  CV_URL: 'https://github.com/abdeelfarouah/my_presentation/blob/Master/src/abderrahmane%20EL%20FAROUAH%20CV.pdf',
 };
 export const NAV_ITEMS = [
   { href: '#home', label: 'Accueil' },
